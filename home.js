587ff26574e16976a62ef3e81129361977fcc9c8
AJT8112 <alexnderJT@hotmail.com>
2022-01-11 23:14:56 +0000

    Variable PluralSight
    Reinforcement of variable skills knowledge from PS.

--- home.js
+++ home.js
@@ -1,2 +1,40 @@
-alert("Hello World!");
-showMessage('Get a Grip');
+let message = "Hello",
+price = 49.99,
+product = "Hiking boots",
+discounted = false;
+
+message += " World!"
+
+message = message.toUpperCase();
+
+message = message.substring(1);
+
+//price += 1;
+
+//price = 27.99;
+
+//price = 19.99;
+
+alert(typeof discounted);
+//showMessage(message);
+//showMessage(price);
+console.log(product);
+
+//CONVERT FROM NUMBER TO STRING DATA TYPE.
+
+/*let amount = 123;
+amount = amount.toString();
+showMessage(amount); */
+
+//CONVERT FROM STRING TO NUMBER DATA TYPE.
+
+/* let amount = Number.parseFloat("123.45");
+showMessage(typeof amount); */
+
+//Objects
+
+let person = {
+    firstName: "Alex",
+    secondName: "Thomas"
+};
+showMessage(person.firstName);
